fae455cd289843745f6fdc28bcc5bf484c8b8760
Kayla Webb <kaylarwebb@gmail.com>
2017-12-22 21:10:46 -0500

    Adding components for the landing page and navigation

---
+++ client/src/components/landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './header'
+
+export default class Landing extends React.Component {
+  render() {
+    <main>
+      <Header />
+      <Navigation />
+
+
+    </main>
+  }
+}
