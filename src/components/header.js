2c722d0e8e147cf63eacb19379782b1bce6bc888
Kayla Webb <kaylarwebb@gmail.com>
2017-12-23 09:05:55 -0500

    Add gitignore, images folder

--- src/components/header.js
+++ src/components/header.js
@@ -4,8 +4,8 @@ import './header.css'
 
 export default function Header() {
   return (
-    <header className='bizName'>
-      <span id='kd'>kd</span><span id='kdesign'>kayladesign</span>
+    <header className='bizName col-12'>
+      <span  id='kd'>kd</span><span id='kdesign'>kayladesign</span>
     </header>
   )
 }
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,8 +6,9 @@ export default function Navigation() {
   return (
   <div className='navSpacer col-3'>
     <Header />
-    <nav>
+    <nav> 
       <ul className='navList col-3'>
+      <img id='logomain' src='../images/kd-logo'/>
         <li className='navAbout'><a id='about' href='#'>About</a></li>
         <li className='navServ'><a id='services' href='#'>Services</a></li>
         <li className='navPort'><a id='portfolio' href='#'>Portfolio</a></li>
